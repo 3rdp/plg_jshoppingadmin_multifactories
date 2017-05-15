c7aec18a10d622600732cab96823af7afa465995
Sasha XUbuntu <third.pancake16@gmail.com>
2017-05-15 23:08:23 +0300

    Display price fields in products.edit

--- multifactories.php
+++ multifactories.php
@@ -27,8 +27,28 @@ class plgJshoppingAdminMultifactories extends JPlugin{
 	 * @since   1.5
 	 * @link https://docs.joomla.org/Plugin/Events/User#onUserAuthenticate
 	 */
-	public function onBeforeDisplayEditProduct($credentials, $options, &$response){
-
-        var_dump($credentials);
-	}
+    function onDisplayProductEditTabsEndTab() {
+        ?> <li><a href="#hello" data-toggle="tab">Lol</a></li> <?php
+    }
+    function onDisplayProductEditTabsEnd() {
+        ?> <div id="hello" class="tab-pane "><table><?php
+        $this->getHtmlFactories();
+        ?></table></div> <?php
+    }
+    private function getHtmlFactories() {
+        $db = JFactory::getDbo();
+        $dbname = $db->quoteName('#__multifactories_crudfactories');
+        $query = "SELECT * FROM $dbname";
+        $db->setQuery($query);
+        $result = $db->loadObjectList();
+        // var_dump($result);
+        // var_dump($dbname);
+        foreach ($result as $factory) {
+?>
+<tr>
+        <input class="inputbox wide" type="text" placeholder="<?=$factory->alias?>" />
+</tr>
+<?php
+        }
+    }
 }
